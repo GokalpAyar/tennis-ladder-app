90295eaa2631ff38c10d24fa8e106e415377af72
Gokalp Ayar <gokalpayar@gmail.com>
2026-05-21 17:26:30 -0400

    dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './app/AuthProvider';
-import { NotificationsProvider } from './app/NotificationsProvider';
 import App from './app/App';
 import './styles/index.css';
 
@@ -10,9 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <NotificationsProvider>
-          <App />
-        </NotificationsProvider>
+        <App />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
